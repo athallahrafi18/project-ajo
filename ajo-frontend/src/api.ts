1d545e4b903afd562b22c15f8ae4c702dfb231cd
mrafiiathallah <athallahrafi181@gmail.com>
2025-05-22 08:41:37 +0700

    edit url menu

--- ajo-frontend/src/api.ts
+++ ajo-frontend/src/api.ts
@@ -1,6 +1,6 @@
 export const fetchMenus = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/menus');
+      const response = await fetch('https://project-ajo-production.up.railway.app/api/menus');
       if (!response.ok) {
         throw new Error('Failed to fetch menus');
       }
